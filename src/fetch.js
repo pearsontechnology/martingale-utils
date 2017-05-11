e4050d144f8297f5122a8fe00cb419444f20457b
jdarling <jeremy.darling@gmail.com>
2017-05-11 12:02:33 -0500

    Better fetch options

--- src/fetch.js
+++ src/fetch.js
@@ -4,27 +4,52 @@ import {
 } from './utils';
 
 const makeFetchOptions = (...options)=>{
-  return merge({credentials: 'same-origin'}, ...options);
+  return merge({
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }, ...options);
 };
 
-const fetchJson=({url, callback, ...options})=>{
-  return fetch(url, makeFetchOptions(options))
+const encodePayload = (payload)=>{
+  if(typeof(payload)==='string'){
+    try{
+      const json = JSON.parse(payload);
+      return payload;
+    }catch(e){
+      return JSON.stringify(payload);
+    }
+  }
+  return JSON.stringify(payload);
+};
+
+const fetchJson=({url, callback, payload, ...options})=>{
+  return fetch(url, makeFetchOptions(options, {body: encodePayload(payload)}))
     .then((response)=>{
-      response.json()
+      const {
+        headers
+      } = response;
+      const contentType = headers.has('Content-Type')?headers.get('Content-Type'):'text/plain';
+      if(contentType.match('json')){
+        return response.json()
         .then((json)=>{
-          return callback(null, json, response);
+          return callback(null, json, response, contentType);
         })
         .catch((err)=>callback(err));
+      }
+      return response.text()
+      .then((respText)=>{
+        return callback(null, respText, response, contentType);
+      })
+      .catch((err)=>callback(err));
     })
     .catch((err)=>callback(err));
 };
 
 const postJson=({method="post", body, payload, ...options})=>{
   const jsonBody = JSON.stringify(payload || body);
-  const headers = {
-    'Content-Type': 'application/json'
-  };
-  fetchJson(makeFetchOptions(options, {method, body: jsonBody, headers}));
+  fetchJson(makeFetchOptions(options, {method, body: jsonBody}));
 };
 
 module.exports = {
